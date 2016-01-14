89a618c64f7e9e19b0d05db8090b16a11e472965
[acarvjl] <acarvjl@oulook.com>
2016-01-13 20:55:42 -0800

    ending day2

---
+++ app.js
@@ -0,0 +1,11 @@
+alert("Let's play a game now!!!");
+
+var userName = prompt("What is your First Name?");
+console.log(userName);
+
+var lastName = prompt("What is your Last Name?");
+console.log(lastName);
+
+alert(lastName);
+
+var userChoce = confirm("We are endding this session?");
---
+++ myjsfile.js
@@ -0,0 +1,11 @@
+alert("I'm external");
+
+//tell the user something
+alert("Ah, hi there, I'll wait here till you close me");
+
+//confirm a user’s action
+var userChoice = confirm("Sure you want to do that...?");
+console.log(userChoice);
+//ask the user for input
+var username = prompt("What is your name?");
+console.log(username);
